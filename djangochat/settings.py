1ab5be5d08158bdc773453526b135ad79608fbc9
Thomas Vu <samduykhanh01@gmail.com>
2024-04-05 16:00:33 -0400

    Made changes so it works on vercel hosting

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-=3)88@*@fx$p%iz=xac&do4-1=h20^t)yy(t9_173g6t==lalc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
